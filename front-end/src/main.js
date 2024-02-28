02c3aea9a4f83dfba052358b925e323938fef8a2
michibou <mourchid.moutuidine@hotmail.com>
2024-02-28 16:12:39 +0100

    TP: CSS Loader finished

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,6 +4,8 @@ import { WelcomeComponent } from "./app/scripts/welcome";
 import { GameComponent } from "./app/scripts/game";
 import { ScoreComponent } from "./app/scripts/score";
 // TODO #import-css: use ES side-effect imports to import styles/style.css
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "/src/app/styles/style.css";
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
