ee38a801de45a1bf2006248aba840a91a63f81d5
michibou <mourchid.moutuidine@hotmail.com>
2024-02-28 16:43:49 +0100

    TP: HTML Loader - Optional

--- front-end/src/app/scripts/game.js
+++ front-end/src/app/scripts/game.js
@@ -2,6 +2,17 @@
 import { parseUrl } from "./utils";
 
 import template from "../views/game.html";
+import back from "/src/assets/cards/back.png";
+import card0 from "/src/assets/cards/card-0.png";
+import card1 from "/src/assets/cards/card-1.png";
+import card2 from "/src/assets/cards/card-2.png";
+import card3 from "/src/assets/cards/card-3.png";
+import card4 from "/src/assets/cards/card-4.png";
+import card5 from "/src/assets/cards/card-5.png";
+import card6 from "/src/assets/cards/card-6.png";
+import card7 from "/src/assets/cards/card-7.png";
+import card8 from "/src/assets/cards/card-8.png";
+import card9 from "/src/assets/cards/card-9.png";
 
 var CARD_TEMPLATE = ""
   .concat('<main class="card-cmp">')
@@ -218,17 +229,17 @@ var CARD_TEMPLATE = ""
   // TODO #card-component: Change images location to /app/components/game/card/assets/***.png
   // TODO #import-assets: use ES default import to import images.
   var CARDS_IMAGE = [
-    "/src/assets/cards/back.png",
-    "/src/assets/cards/card-0.png",
-    "/src/assets/cards/card-1.png",
-    "/src/assets/cards/card-2.png",
-    "/src/assets/cards/card-3.png",
-    "/src/assets/cards/card-4.png",
-    "/src/assets/cards/card-5.png",
-    "/src/assets/cards/card-6.png",
-    "/src/assets/cards/card-7.png",
-    "/src/assets/cards/card-8.png",
-    "/src/assets/cards/card-9.png",
+    back,
+    card0,
+    card1,
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    card7,
+    card8,
+    card9,
   ];
 
   // TODO #class: use the ES6 class keyword
--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
           test: /\.(html)$/,
           use: ["html-loader"],
         },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          type: 'asset/resource'
+        }
       ],
   },
 };
